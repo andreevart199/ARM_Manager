c8101f009c3872239d4d7f58adff11a116214b3c
Андреев Артём Сергеевич <AndreevAS@medkhv.ru>
2020-03-24 15:48:54 +1000

    910

--- dipl0611/Controllers/productsController.cs
+++ dipl0611/Controllers/productsController.cs
@@ -17,7 +17,7 @@ namespace dipl0611.Controllers
         // GET: products
         public ActionResult Index()
         {
-            var products = db.products.Include(p => p.id);
+            var products = db.products.Include(p => p.kontragents);
             return View(products.ToList());
         }
 
